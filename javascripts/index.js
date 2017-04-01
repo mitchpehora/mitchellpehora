02435b58813ac22208df2d260050bcf6047ee4d6
mitchell <mitchellpeh@outlook.com>
2017-04-01 19:31:26 -0400

    javascript & stylesheet organization

---
+++ javascripts/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.contactMe').mouseenter(function() {
+        $('.contactMe').fadeTo('fast',1);
+    });
+    $('.contactMe').mouseleave(function() {
+        $('.contactMe').fadeTo('fast',0.75);
+    });
+  }
