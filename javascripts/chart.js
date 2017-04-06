1bee0d57ac71b798880412994c6baeb4a9a6dd8d
mitchpeh <mitchmac@Mitchells-MacBook-Pro.local>
2017-04-06 11:34:47 -0400

    last month
    added last month functionality, tidied up css

--- javascripts/chart.js
+++ javascripts/chart.js
@@ -1,105 +1,23 @@
 
 
 $(document).ready(function() {
-  $('.contactMe').mouseenter(function() {
-      $('.contactMe').fadeTo('fast',1);
+  $('.home').mouseenter(function() {
+      $('.home').fadeTo('fast',1);
   });
-  $('.contactMe').mouseleave(function() {
-      $('.contactMe').fadeTo('fast',0.75);
+  $('.home').mouseleave(function() {
+      $('.home').fadeTo('fast',0.75);
   });
     button1 = document.getElementById('sensor1Button');
     button2 = document.getElementById('sensor2Button');
     reset = document.getElementById('resetButton');
+    month = document.getElementById('pastMonth');
     var canvas = document.getElementById("myChart"), ctx = canvas.getContext("2d");
-
-
-    todayInfo= new Date();
-    yearToday=todayInfo.getFullYear()
-    dayToday=todayInfo.getDay()
-    monthToday=todayInfo.getMonth()
-    dateToday=todayInfo.getDate();
-    lastMonth=monthToday-1
-    daysLastMonth=31
-
-    if (monthToday == 4 || monthToday == 6 || monthToday == 9 || monthToday == 12)
-    {
-      daysLastMonth = 30
-    }
-
-    if (monthToday==2)
-    {
-      daysLastMonth=28
-    }
-
-    if (monthToday==2 && yearToday%4 ==0)
-    {
-      daysLastMonth=29
-    }
-
-
-    /*xDayWeek=[dayToday,dayToday-1,dayToday-2,dayToday-3,dayToday-4,dayToday-5,dayToday-6];*/
-    xDayWeek=[dayToday-6,dayToday-5,dayToday-4,dayToday-3,dayToday-2,dayToday-1,dayToday];
-    xDates=[dateToday-6,dateToday-5,dateToday-4,dateToday-3,dateToday-2,dateToday-1,dateToday]
-    xMonths=[monthToday,monthToday,monthToday,monthToday,monthToday,monthToday,monthToday]
-    xLabels=[]
-
-      for (i = 0; i < xDayWeek.length; i++)
-    {
-
-    if(xDayWeek[i]<0)
-    { xDayWeek[i]+=7; }
-
-    if(xDates[i]==0)
-    {xDates[i]=daysLastMonth; xMonths[i]=monthToday-1;}
-
-    if(xDates[i]==-1)
-    {xDates[i]=daysLastMonth-1; xMonths[i]=monthToday-1;}
-
-    if(xDates[i]==-2)
-    {xDates[i]=daysLastMonth-2; xMonths[i]=monthToday-1;}
-
-    if(xDates[i]==-3)
-    {xDates[i]=daysLastMonth-3; xMonths[i]=monthToday-1;}
-
-    if(xDates[i]==-4)
-    {xDates[i]=daysLastMonth-4; xMonths[i]=monthToday-1;}
-
-    if(xDates[i]==-5)
-    {xDates[i]=daysLastMonth-5; xMonths[i]=monthToday-1;}
-
-
-
-    if (xDayWeek[i]==6)
-    {xDayWeek[i]='Saturday';}
-
-    if (xDayWeek[i]==5)
-    {xDayWeek[i]='Friday ';}
-
-    if (xDayWeek[i]==4)
-    {xDayWeek[i]='Thursday';}
-
-    if (xDayWeek[i]==3)
-    {xDayWeek[i]='Wednesday';}
-
-    if (xDayWeek[i]==2)
-    {xDayWeek[i]='Tuesday';}
-
-    if (xDayWeek[i]==1)
-    {xDayWeek[i]='Monday';}
-
-    if (xDayWeek[i]==0)
-    {xDayWeek[i]='Sunday';}
-
-    xLabels[i]=xDayWeek[i]+' ('+(xMonths[i]+1)+' - '+xDates[i]+')';
-}
-
-
-
-
+    var xMonthLabels=lastMonth()
 
     var blank=[0,0,0,0,0,0,0,];
     var set1=[1,2,3,4,5,6,7];
     var set2=[1,1,1,1,1,1,1,];
+    var monthSet=[1,2,3,4,25,36,44,53,64,51,42,33,24,15,16,17,18,19,20,21,22,23,23,25,26,27,28,29,30];
 
     var ctx = document.getElementById('myChart').getContext('2d');
 
@@ -108,7 +26,7 @@ $(document).ready(function() {
       responsive: false,
       type: 'line',
       data: {
-        labels: xLabels,
+        labels: lastWeek(),
         datasets: [{
           label: 'Temperature',
           data: blank,
@@ -122,7 +40,7 @@ $(document).ready(function() {
     			myChart = new Chart(ctx, {
     			  type: 'line',
     			  data: {
-    			    labels: xLabels,
+    			    labels: lastWeek(),
     			    datasets: [{
     			      label: 'Temperature',
     			      data: set1,
@@ -133,12 +51,14 @@ $(document).ready(function() {
         }
 
 
+
+
 button2.onclick = function() {
 
           			myChart = new Chart(ctx, {
           			  type: 'line',
           			  data: {
-          			    labels: xLabels,
+          			    labels: lastWeek(),
           			    datasets: [{
           			      label: 'Temperature',
           			      data: set2,
@@ -148,12 +68,27 @@ button2.onclick = function() {
           			});
           }
 
+          month.onclick = function() {
+
+                    			myChart = new Chart(ctx, {
+                    			  type: 'line',
+                    			  data: {
+                    			    labels: xMonthLabels,
+                    			    datasets: [{
+                    			      label: 'Temperature',
+                    			      data: monthSet,
+                    			      backgroundColor: "rgba(153,255,51,0.4)"
+                    			    }]
+                    			  }
+                    			});
+                    }
+
           reset.onclick = function() {
                           var set1=[0,0,0,0,0,0,0];
                     			myChart = new Chart(ctx, {
                     			  type: 'line',
                     			  data: {
-                    			    labels: xLabels,
+                    			    labels: lastWeek(),
                     			    datasets: [{
                     			      label: 'Temperature',
                     			      data: blank,
